6a0127e59104704169740f6fd89d74c1918bbbc3
kone <70107937+konekowo@users.noreply.github.com>
2025-01-31 21:14:57 -0500

    get rid of tutorial comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <thread>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-
-/* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = nullptr;
 static SDL_Renderer *renderer = nullptr;
 
-/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
@@ -30,14 +27,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
     SDL_RaiseWindow(window);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     switch (event->type) {
         case SDL_EVENT_QUIT:
-            return SDL_APP_SUCCESS;
         case SDL_EVENT_TERMINATING:
             return SDL_APP_SUCCESS;
         default:;
@@ -45,24 +40,20 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
     SDL_RaiseWindow(window);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    /* clear the window to the draw color. */
+
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderDebugText(renderer, 0, 5, "Hello World!");
 
-    /* put the newly-cleared rendering on the screen. */
     SDL_RenderPresent(renderer);
 
     SDL_Delay(100);
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    /* SDL will clean up the window/renderer for us. */
 }
