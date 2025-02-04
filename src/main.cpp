022c8de75a40a435a672f4412fa1b8d11f93d554
kone <70107937+konekowo@users.noreply.github.com>
2025-02-04 10:26:14 -0500

    add dynamic scaling

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #define USE_TASKBAR_LEFT_POSITION false
-#define SCALE 1.0f
-#define WINDOW_WIDTH (int) std::round((250*SCALE))
-#define WINDOW_HEIGHT (int) std::round((47*SCALE))
 #define FETCH_TRIES 50
 
 #include <SDL3/SDL.h>
@@ -20,10 +17,15 @@ using json = nlohmann::json;
 static SDL_Window *window = nullptr;
 static SDL_Renderer *renderer = nullptr;
 
+static float scale = 1.0f;
+static int windowWidth = static_cast<int>(std::round(250 * scale));
+static int windowHeight = static_cast<int>(std::round(47 * scale));
 static int windowX = 0;
 static int windowY = 0;
 static int currentWinX;
 static int currentWinY;
+static int currentWinWidth;
+static int currentWinHeight;
 static int fetchTry = 0;
 static int elipsesCount = 0;
 static int elipsesTimer = 0;
@@ -33,14 +35,18 @@ static SDL_Color fontColorSeconds = {255, 255, 255, 100};
 static Schedule *schedule = nullptr;
 static TextManager *textManager = nullptr;
 
-void CalculateWindowPos(SDL_Window *window) {
+
+void CalculateWindowPosAndSize(SDL_Window *window) {
+    windowWidth = static_cast<int>(std::round(250 * scale));
+    windowHeight = static_cast<int>(std::round(47 * scale));
+
     const SDL_DisplayMode *displayMode = SDL_GetCurrentDisplayMode(SDL_GetDisplayForWindow(window));
 #if (defined(USE_TASKBAR_LEFT_POSITION) && USE_TASKBAR_LEFT_POSITION == true)
     windowX = *const_cast<int *>(&displayMode->w) - 550;
 #else
     windowX = 0;
 #endif
-    windowY = static_cast<int>(std::round(static_cast<float>(displayMode->h) - WINDOW_HEIGHT));
+    windowY = static_cast<int>(std::round(static_cast<float>(displayMode->h) - static_cast<float>(windowHeight)));
 }
 
 void FetchSchedule() {
@@ -80,9 +86,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetHint(SDL_HINT_FORCE_RAISEWINDOW, "true");
     SDL_SetHint(SDL_HINT_APP_NAME, "Crooms Bell Schedule");
 
-    if (!SDL_CreateWindowAndRenderer("Crooms Bell Schedule", WINDOW_WIDTH, WINDOW_HEIGHT,
+    if (!SDL_CreateWindowAndRenderer("Crooms Bell Schedule", windowWidth, windowHeight,
                                      SDL_WINDOW_TRANSPARENT | SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP |
-                                             SDL_WINDOW_NOT_FOCUSABLE,
+                                             SDL_WINDOW_NOT_FOCUSABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY,
                                      &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -96,8 +102,10 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
     textManager = new TextManager(renderer);
 
-    CalculateWindowPos(window);
+    scale = SDL_GetWindowDisplayScale(window);
+    CalculateWindowPosAndSize(window);
 
+    SDL_SetWindowSize(window, windowWidth, windowHeight);
     SDL_SetWindowPosition(window, windowX, windowY);
 
     if (std::string(SDL_GetPlatform()) == "Windows") {
@@ -126,21 +134,30 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     if (std::string(SDL_GetPlatform()) == "Windows") {
         SDL_RaiseWindow(window);
     }
-    CalculateWindowPos(window);
+
+    scale = SDL_GetWindowDisplayScale(window);
+    CalculateWindowPosAndSize(window);
+    SDL_SetWindowSize(window, windowWidth, windowHeight);
     if (SDL_GetWindowPosition(window, &currentWinX, &currentWinY)) {
-        if (currentWinX != windowX && currentWinY != windowY) {
+        if (currentWinX != windowX || currentWinY != windowY) {
             SDL_SetWindowPosition(window, windowX, windowY);
             SDL_Log("Window moved back to correct position");
         }
     }
-    SDL_SetWindowPosition(window, windowX, windowY);
+    if (SDL_GetWindowSize(window, &currentWinWidth, &currentWinHeight)) {
+        if (currentWinWidth != windowWidth || currentWinHeight != windowHeight) {
+            SDL_SetWindowSize(window, windowWidth, windowHeight);
+            SDL_Log("Window scaled to correct scale");
+        }
+    }
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    const SDL_FRect dimensions = textManager->RenderText(currentFont, "dimensionText", "A", INT32_MAX, INT32_MAX, fontColor, 0.43f * SCALE);
+    // ReSharper disable once CppUseStructuredBinding
+    const SDL_FRect dimensions = textManager->RenderText(currentFont, "dimensionText", "A", INT32_MAX, INT32_MAX, fontColor, 0.43f * scale);
 
     if (schedule != nullptr) {
         const std::string dayType = schedule->GetData().msg;
@@ -151,12 +168,12 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         const int minsLeft = (timeLeft - hoursLeft * 60 * 60) / 60;
         const int secsLeft = timeLeft - minsLeft * 60 - hoursLeft * 60 * 60;
 
-        const SDL_FRect dayTypeText = textManager->RenderText(currentFont, "display.dayType", dayType, 10, WINDOW_HEIGHT - 6 - dimensions.h * 2, fontColor, 0.43f * SCALE);
+        const SDL_FRect dayTypeText = textManager->RenderText(currentFont, "display.dayType", dayType, 10, static_cast<float>(windowHeight) - 6 - dimensions.h * 2, fontColor, 0.43f * scale);
 
         // ReSharper disable once CppUseStructuredBinding
         const SDL_FRect eventName =
                 textManager->RenderText(currentFont, "display.classTimeLeft.eventName",
-                    event, 10, WINDOW_HEIGHT - 7 - dayTypeText.h, fontColor, 0.43f * SCALE);
+                    event, 10, static_cast<float>(windowHeight) - 7 - dayTypeText.h, fontColor, 0.43f * scale);
 
 
         std::string hrsMins;
@@ -170,19 +187,19 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         // ReSharper disable once CppUseStructuredBinding
         const SDL_FRect hrsMinsDimensions =
                 textManager->RenderText(currentFont, "display.classTimeLeft.HrsMins", hrsMins, eventName.x + eventName.w,
-                                        eventName.y, fontColor, 0.43f * SCALE);
+                                        eventName.y, fontColor, 0.43f * scale);
 
         const char *secs = (":" + Sched_PadTime(secsLeft, 2)).c_str();
 
         textManager->RenderText(currentFont, "display.classTimeLeft.Seconds", secs,
-                                hrsMinsDimensions.x + hrsMinsDimensions.w, hrsMinsDimensions.y, fontColorSeconds, 0.43f * SCALE);
+                                hrsMinsDimensions.x + hrsMinsDimensions.w, hrsMinsDimensions.y, fontColorSeconds, 0.43f * scale);
         SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 100);
-        const auto progressBarBG = SDL_FRect{0, static_cast<float>(WINDOW_HEIGHT) - SCALE * 2, static_cast<float>(WINDOW_WIDTH), SCALE * 2};
+        const auto progressBarBG = SDL_FRect{0, static_cast<float>(windowHeight) - scale * 2, static_cast<float>(windowWidth), scale * 2};
         SDL_RenderFillRect(renderer, &progressBarBG);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        const auto progressBar = SDL_FRect{0, static_cast<float>(WINDOW_HEIGHT) - SCALE * 2,
-            static_cast<float>(WINDOW_WIDTH) * (static_cast<float>(eventTime - timeLeft) / static_cast<float>(eventTime)), SCALE * 2};
+        const auto progressBar = SDL_FRect{0, static_cast<float>(windowHeight) - scale * 2,
+            static_cast<float>(windowWidth) * (static_cast<float>(eventTime - timeLeft) / static_cast<float>(eventTime)), scale * 2};
         SDL_RenderFillRect(renderer, &progressBar);
     } else {
         std::string loadingText = "Fetching Schedule";
@@ -190,7 +207,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
             loadingText += ".";
         }
         textManager->RenderText(currentFont, "display.loading", loadingText,
-            10, WINDOW_HEIGHT - 7 - dimensions.h, fontColor, 0.43f * SCALE);
+            10, static_cast<float>(windowHeight) - 7 - dimensions.h, fontColor, 0.43f * scale);
         elipsesTimer += 200;
         if (elipsesTimer > 400) {
             elipsesCount++;
